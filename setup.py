332a60be798e157599ce23b35c6658dce2766218
fynnbe <thefynnbe@gmail.com>
2019-06-14 09:30:32 +0200

    remove dependencies

--- setup.py
+++ setup.py
@@ -10,13 +10,7 @@ setup(
     license="Apache License 2.0",
     packages=["template_experiment_models"],
     install_requires=[
-        "torch=1.1.0",
-        "dill=0.2.9",
-        "pyyaml=5.1",
-        "scipy=1.2.1",
-        "h5py=2.9.0",
-        "numpy=1.16.3",
-        "scikit-image=0.15.0",
+        "pytorch",
     ],
     zip_safe=False,
 )
